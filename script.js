d3c45454281f6d7700708636961c4aa257699c44
MCodes96 <mikayil_gacek@outlook.com>
2022-07-10 15:36:36 +0200

    Add playRound function to script.js and define game logic inside of it

--- script.js
+++ script.js
@@ -11,7 +11,27 @@ function computerPlay() {
   }
 }
 
-//Create function that takes Player and CPU Choice and plays a single round of Rock, Paper, Scissors
+// Create function that takes Player and CPU Choice and plays a single round of Rock, Paper, Scissors
 function playRound(playerSelection, computerSelection) {
-  computerSelection = computerPlay();
+  computerSelection = computerPlay().toLowerCase();
+  playerSelection = prompt(
+    "Choose and type one: 'Rock', 'Paper' or 'Scissors'"
+  ).toLowerCase();
+
+  // Define Draw
+  if (playerSelection === computerSelection) {
+    return 'Draw!';
+  }
+  // Define Win cases for the player
+  else if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    return 'Win!';
+  }
+  // Define Loss cases for the player
+  else {
+    return 'Loss!';
+  }
 }
